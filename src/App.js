af554791a9fd125f1a86e9fb645cdbd34635769a
Joao Souza <joaohsistemas@gmail.com>
2018-05-13 00:57:17 -0300

    Estrutura basica do header

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+import Header from './components/header';
+
+const App = () => (
+    <Header />
+);
+
+export default App;
