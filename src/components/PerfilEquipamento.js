e85eadc7931508d13161c2b1647ca9121ec5968a
Joao Souza <joaohsistemas@gmail.com>
2018-06-19 18:14:22 -0300

    Ajuste no layout

--- src/components/PerfilEquipamento.js
+++ src/components/PerfilEquipamento.js
@@ -95,6 +95,7 @@ const styles = StyleSheet.create ({
         justifyContent: 'space-around',
         alignItems: 'center',
         backgroundColor: 'white',
+        textAlign: 'left',
         flex: 1
     },
     button: {
